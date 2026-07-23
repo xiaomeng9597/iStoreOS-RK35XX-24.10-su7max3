2b52e12b2c4f240e79b8c645f4d89bf27a9a42a7
xiaomeng9597 <519561910@qq.com>
2026-07-24 02:36:20 +0800

    同步上游

---
+++ diy-part2-6.x.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#===============================================
+# Description: DIY script
+# File name: diy-script.sh
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#===============================================
+
+# 修改uhttpd配置文件，启用nginx
+# sed -i "/.*uhttpd.*/d" .config
+# sed -i '/.*\/etc\/init.d.*/d' package/network/services/uhttpd/Makefile
+# sed -i '/.*.\/files\/uhttpd.init.*/d' package/network/services/uhttpd/Makefile
+sed -i "s/:80/:81/g" package/network/services/uhttpd/files/uhttpd.config
+sed -i "s/:443/:4443/g" package/network/services/uhttpd/files/uhttpd.config
+cp -a $GITHUB_WORKSPACE/configfiles/etc/* package/base-files/files/etc/
+# ls package/base-files/files/etc/
+
+
+# 追加自定义内核配置项
+echo "CONFIG_PSI=y
+CONFIG_KPROBES=y" >> target/linux/rockchip/armv8/config-6.6
+
+
+# 集成CPU性能跑分脚本
+cp -f $GITHUB_WORKSPACE/configfiles/coremark/coremark-arm64 package/base-files/files/bin/coremark-arm64
+cp -f $GITHUB_WORKSPACE/configfiles/coremark/coremark-arm64.sh package/base-files/files/bin/coremark.sh
+chmod 755 package/base-files/files/bin/coremark-arm64
+chmod 755 package/base-files/files/bin/coremark.sh
+
+
+# 复制dts设备树文件到指定目录下
+cp -a $GITHUB_WORKSPACE/configfiles/dts/rk3588/* target/linux/rockchip/dts/rk3588/
+
+
+# iStoreOS-settings
+git clone --depth=1 -b main https://github.com/xiaomeng9597/istoreos-settings package/default-settings
+
+
+# 定时限速插件
+git clone --depth=1 https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
+
+
+
+# 增加bdy_g98-nas
+echo -e "\\ndefine Device/bdy_g98-nas
+\$(call Device/Legacy/rk3588,\$(1))
+  DEVICE_VENDOR := BDY
+  DEVICE_MODEL := G98 NAS
+  DEVICE_PACKAGES += kmod-r8169 kmod-nvme kmod-ata-ahci-dwc kmod-hwmon-pwmfan kmod-thermal
+endef
+TARGET_DEVICES += bdy_g98-nas" >> target/linux/rockchip/image/legacy.mk
+
+
+# 复制 02_network 网络配置文件到 target/linux/rockchip/armv8/base-files/etc/board.d/ 目录下
+cp -f $GITHUB_WORKSPACE/configfiles/02_network target/linux/rockchip/armv8/base-files/etc/board.d/02_network
+
+
+cp -a $GITHUB_WORKSPACE/configfiles/driver/* target/linux/generic/files
+ls target/linux/generic/files
+
+
+cp -f $GITHUB_WORKSPACE/configfiles/driver/999-net-dsa-add-yt921x-header-defs.patch target/linux/rockchip/patches-6.6/999-net-dsa-add-yt921x-header-defs.patch
+
+
+# cp -f $GITHUB_WORKSPACE/configfiles/driver/010-add-yt921x-tag-driver.patch target/linux/rockchip/patches-6.6/010-add-yt921x-tag-driver.patch
+
+
+# cp -f $GITHUB_WORKSPACE/configfiles/driver/dsa/011-net-dsa-add-tag_yt921x-source.patch target/linux/rockchip/patches-6.6/011-net-dsa-add-tag_yt921x-source.patch
+# cp -f $GITHUB_WORKSPACE/configfiles/driver/Makefile3 target/linux/rockchip/Makefile
